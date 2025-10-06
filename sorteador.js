d7f947b26bb605059fa00e82ccbd78f9f9a5f8b4
weslleysilva8 <weslleygabriel539@gmail.com>
2025-10-06 10:01:14 -0300

    teste

--- sorteador.js
+++ sorteador.js
@@ -3,8 +3,8 @@ const drawButton = document.querySelector(".drawButton");
 function generateNumber (){
     const min = Math.ceil (document.querySelector(".input-min").value)
     const max = Math.floor (document.querySelector(".input-max").value)
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(min, max)
+    const result = Math.floor (Math.random() * (max - min + 1)) + min
+    alert(result)
 }
 drawButton.addEventListener("click", generateNumber)
  
