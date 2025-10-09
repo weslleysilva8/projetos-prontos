9df31904ff38b3febe54ec97a9f6bdb5c4440cce
weslleysilva8 <weslleygabriel539@gmail.com>
2025-10-09 14:43:27 -0300

    aprendizado

--- sorteador.js
+++ sorteador.js
@@ -1,10 +1,14 @@
 const drawButton = document.querySelector(".drawButton");
+let sorteado = false
 
 function generateNumber (){
     const min = Math.ceil (document.querySelector(".input-min").value)
     const max = Math.floor (document.querySelector(".input-max").value)
     const result = Math.floor (Math.random() * (max - min + 1)) + min
-    alert(result)
+    document.getElementById("Resultado").innerHTML = `O número sorteado foi: <strong>${result}</strong>`
+    if (!sorteado) {
+        drawButton.innerHTML = "Sortear novamente"
+    }
 }
 drawButton.addEventListener("click", generateNumber)
  
